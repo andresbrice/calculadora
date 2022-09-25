772e898909d85a0e2300aff374a529960342d02a
andres <andres.martin.bricenio@gmail.com>
2022-09-25 15:11:56 -0300

    primer entrega

---
+++ js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//-----Mensaje de bienvenida-----
+const bienvenida = alert(
+  "Bienvenido al simulador de calculadora by Andrés Briceño. Para su correcto uso deberá ingresar dos números y luego indicar que operación quiere realizar."
+);
+
+//-----Inicio calculadora-----
+calculadora();
